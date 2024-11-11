1b272f6c3b1c7ee5cf9fbe1e767513ab3a2b3b93
SamnangKong426 <132801830+SamnangKong426@users.noreply.github.com>
2024-11-11 08:51:57 +0700

    using getter

--- W4-S3-PRACTICE/EX-1/part2.dart
+++ W4-S3-PRACTICE/EX-1/part2.dart
@@ -6,11 +6,12 @@ void main() => runApp(MaterialApp(
           title: const Text("Custom buttons"),
         ),
         body: const Center(
-          child: SelectableButton(
-            status: "Not selected",
-            textLabel: "Not selected",
-            textColor: Colors.white,
-            bgColor: Colors.grey,
+          child: Column(
+            children: [
+              SelectableButton(
+                status: false,
+              ),
+            ],
           ),
         ),
       ),
@@ -20,50 +21,27 @@ class SelectableButton extends StatefulWidget {
   const SelectableButton({
     super.key,
     required this.status,
-    required this.textLabel,
-    required this.textColor,
-    required this.bgColor,
   });
 
-  final String status;
-  final String textLabel;
-  final Color textColor;
-  final Color bgColor;
+  final bool status;
 
   @override
   State<SelectableButton> createState() => _SelectableButtonState();
 }
 
 class _SelectableButtonState extends State<SelectableButton> {
-  late String status;
-  late String textLabel;
-  late Color textColor;
-  late Color bgColor;
-
-  @override
-  void initState() {
-    super.initState();
-    status = widget.status;
-    textLabel = widget.textLabel;
-    textColor = widget.textColor;
-    bgColor = widget.bgColor;
-  }
-
+  bool isSelected = false;
+  
   void toggleStatus() {
     setState(() {
-      if (status == "Not selected") {
-        status = "Selected";
-        textLabel = "Selected";
-        textColor = Colors.white;
-        bgColor = Colors.blue;
-      } else {
-        status = "Not selected";
-        textLabel = "Not selected";
-        textColor = Colors.black;
-        bgColor = Colors.blue.shade50;
-      }
+      isSelected = !isSelected;
     });
   }
+
+  String get textLabel => isSelected ? 'Selected' : 'Not selected';
+  Color? get textColor => isSelected ? Colors.white : Colors.black;
+  Color? get bgColor => isSelected ? Colors.blue[500] : Colors.blue[50];
+
 
   @override
   Widget build(BuildContext context) {
@@ -78,7 +56,7 @@ class _SelectableButtonState extends State<SelectableButton> {
         child: Center(
           child: Text(
             textLabel,
-            style: TextStyle(color: widget.textColor, fontSize: 20),
+            style: TextStyle(color: textColor, fontSize: 20),
           ),
         ),
       ),
