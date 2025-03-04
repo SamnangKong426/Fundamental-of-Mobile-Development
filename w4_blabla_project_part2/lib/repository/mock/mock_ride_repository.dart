75041be13715f992c052e27a641586511168cd2a
SamnangKong426 <samnangkong499@gmail.com>
2025-03-04 15:26:24 +0700

    BLA-103 – BONUS – Provide the ride with a sorting mode

--- w4_blabla_project_part2/lib/repository/mock/mock_ride_repository.dart
+++ w4_blabla_project_part2/lib/repository/mock/mock_ride_repository.dart
@@ -41,27 +41,47 @@ class MockRideRepository implements RidesRepository {
   ];
 
   final List<RidesFilter> _ridesFilter = [
-    RidesFilter(
-      petsAccepted: false,
-    ),
-        RidesFilter(
-      petsAccepted: false,
-    ),
-        RidesFilter(
-      petsAccepted: false,
-    ),
-        RidesFilter(
-      petsAccepted: true,
-    ),
-        RidesFilter(
-      petsAccepted: false,
-    ),
-    
+    RidesFilter(petsAccepted: false),
+    RidesFilter(petsAccepted: false),
+    RidesFilter(petsAccepted: false),
+    RidesFilter(petsAccepted: true),
+    RidesFilter(petsAccepted: false),
   ];
 
   @override
-  List<Ride> getRides(RidePreference preference, RidesFilter? filter) {
+  List<Ride> getRides(
+    RidePreference preference,
+    RidesFilter? filter,
+    RideSortType? sortType,
+  ) {
     final ridesService = RidesService();
-    return ridesService.getRidesFor(preference);
+
+    // Get filtered rides based on preference
+    List<Ride> rides = ridesService.getRidesFor(preference);
+
+    // Apply sorting if sort type is provided
+    if (sortType != null) {
+      switch (sortType) {
+        case RideSortType.departure:
+          rides.sort(
+            (a, b) =>
+                a.departureLocation.name.compareTo(b.departureLocation.name),
+          );
+          break;
+        case RideSortType.departureDate:
+          rides.sort((a, b) => a.departureDate.compareTo(b.departureDate));
+          break;
+        case RideSortType.arrival:
+          rides.sort(
+            (a, b) => a.arrivalLocation.name.compareTo(b.arrivalLocation.name),
+          );
+          break;
+        case RideSortType.requestedSeats:
+          rides.sort((a, b) => b.availableSeats.compareTo(a.availableSeats));
+          break;
+      }
+    }
+
+    return rides;
   }
 }
--- w4_blabla_project_part2/lib/service/rides_service.dart
+++ w4_blabla_project_part2/lib/service/rides_service.dart
@@ -2,6 +2,15 @@ import '../dummy_data/dummy_data.dart';
 import '../model/ride/ride.dart';
 import '../model/ride_pref/ride_pref.dart';
 
+
+/// Enum defining different sorting criteria for rides
+enum RideSortType {
+  departure,      // Sort by departure location
+  departureDate,  // Sort by departure date and time
+  arrival,        // Sort by arrival location
+  requestedSeats, // Sort by number of requested seats
+}
+
 ////
 ///   This service handles:
 ///   - The list of available rides
